bbc97b60805717ce48d7253c70a38f64a6a399b4
Ella Stone <estone@eckerd.edu>
2025-03-24 22:52:22 -0400

    frontend ella

--- extensions.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-#Creating db isntance
-db = SQLAlchemy()
-
-# By moving db initialization to a separate file (extensions.py)
-# this break the circular dependency of previous db initializaing in app.py
